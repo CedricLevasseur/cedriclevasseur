3948125e2333eea8eea565af376bf29b41ab166d
root <root@localhost.(none)>
2010-12-10 17:45:04 +0100

    big endhancement

--- checker/Checker.groovy
+++ checker/Checker.groovy
@@ -1,57 +1,46 @@
-package checker
+package vidal
 
+import org.htmlparser.*
+import org.htmlparser.util.*
+import org.htmlparser.filters.*
+import org.htmlparser.tags.*
 
-
-/**
- * Check the size of a file exposed on the network via http or on the filesystem.
- *
- * @author Cédric Levasseur
- */
 public class Checker{
 
 
+    private String url;
+    private Integer size;
+
     private float errorPercent=0.05;
-
-    public static FILE_NOT_FOUND_OR_EMPTY = -1;
-    public static FILE_SIZE_VALID = 0;
-    public static FILE_SIZE_EXPECTED_INCORRECT = -2;
-
-
-
-
     public  ConfigObject config;
 
-
-    /**
-     * @param float the percent of size permitted :
-     * 0.05 means the size could differs of 5%
-     */
     public setErrorPercent(float percent){
 	errorPercent=percent;
     }	
-
-    /**
-     *
-     * @return the error percent
-     */
+    
     public float getErrorPercent(){
 	return errorPercent;
     }	
 
-
-    /**
-     * the constructor load the config File
-     */
     public Checker(){
  	load();	
     }
     
-    /**
-     * Convert a numbers of bytes into human reading size
-     * in example : 1024 is 1K
-     * @param a number of bytes
-     */
-    private static String roundMe(long bytesNumber){
+    public static FILE_NOT_FOUND_OR_EMPTY = -1;
+    public static FILE_SIZE_VALID = 0;
+    public static FILE_SIZE_EXPECTED_INCORRECT = -2;
+
+    public void setUrl(String _url){
+    	url=_url;
+    }
+    public String getUrl(){
+    	return url;
+    }
+    public void setSize(Integer _size){
+    	size=_size;
+    }
+
+    public static String roundMe(long bytesNumber){
 
         def unites=["b","K","M","G","T"]
         def iter=0;
@@ -62,75 +51,49 @@ public class Checker{
         return bytesNumber.toString() + unites[iter];
     }
 
-    /**
-     *
-     *
-     */
-
     public Object getConfig(){
 	return config;
     }
 
 
-    /**
-     * This main method check ALL the files specified into the SizeConfig.groovy
-     *
-     */
-
     public static void main(String[] args){
 
         def checker = new Checker();
 
-        checker.diskCheck();
 	for (e in checker.config){
-		def url=e.getValue().get('url');
-		def size=e.getValue().get('size');
+		checker.setUrl(e.getValue().get('url'));
+		checker.setSize(e.getValue().get('size'));
 
 		int res=0;
-		switch (res=checker.fileCheck(url, size)){
+		switch (res=checker.fileCheck()){
 			case FILE_NOT_FOUND_OR_EMPTY :
-	            		System.out.println("File Not Found or empty : "+e.getValue().get('url'));
+	            		System.out.println("File Not Found or empty : "+checker.getUrl());
 				break;
 			case FILE_SIZE_EXPECTED_INCORRECT :
-	            		System.out.println("Expected File Size is invalid : "+e.getValue().get('url'));
+	            		System.out.println("Expected File Size is invalid : "+checker.getUrl());
 				break;
 			case FILE_SIZE_VALID :
-	            		System.out.println("CONFORME : "+e.getValue().get('url'));
+	            		System.out.println("CONFORME : "+checker.getUrl());
 				break;
 			default :
-				System.out.println("FILE SIZE NOT CONFORME, error=" + res +"%");
+				System.out.println("FILE SIZE NOT CONFORME, error=" + res +"% : "+checker.getUrl() );
 		}
     	}
     }
 
-    /**
-     * load the list of file from checker/SizeConfig.groovy
-     */
-    
     private void load(){
 
-	config = new ConfigSlurper().parse(new File('checker/SizeConfig.groovy').toURL())
+	config = new ConfigSlurper().parse(new File('vidal/SizeConfig.groovy').toURL())
     }
-
-    /**
-     * @deprecated
-     *
-     */
 
     private void save(){
 
-	new File("checker/SizeConfig.groovy").withWriter { writer ->config.writeTo(writer)}
+	new File("vidal/Size.groovy").withWriter { writer ->config.writeTo(writer)}
     }
-
-
-    /**
-     * Print out the place on the disk
-     * To be continued
-     */
 
     public void diskCheck(){
         //File f = new File("c:/");
-        File f = new File("/");
+        File f = new File("F:/");
         // prints the volume size in bytes.
         System.out.println("Total Space="+Checker.roundMe(f.getTotalSpace()));
         // prints the total free bytes for the volume in bytes.
@@ -143,27 +106,60 @@ public class Checker{
     }
 
 
-    /**
-     * check a file
-     * @param src a location of a file, either http:// or a path (in java syntax which means which slash like c:/directory/file )
-     * @param fileLenghtExpected a size in number of bytes
-     */
-    public int fileCheck(String src, long fileLengthExpected ){
-	if(url.startsWith("http")){
-		return fileHttpCheck(src,fileLengthExpected);
-	}else{
-		return fileFsCheck(src,fileLengthExpected);
+    private int fileCheck(){
+
+	if(url.contains("*")){
+		url=fileLastMatch(url);
+	}
+	if(url!=null && url.size()>0 ) {		
+		if(url.startsWith("http")){
+			return fileHttpCheck(url,size);
+		}else{
+			return fileFsCheck(url,size);
+		}
 	}
     }
 
+    private String fileLastMatch(String url){
 
-    /**
-     * check a file exposed via http
-     * @param src a location of a file, on  http://
-     * @param fileLenghtExpected a size in number of bytes
-     */
+            String prefix=url.substring(url.lastIndexOf('/')+1,url.lastIndexOf('*'));
+	    if(url.startsWith("http")){
+	    	String directory= url.substring(0,url.lastIndexOf('/'));
+		Parser parser = new Parser(directory);
+		//NodeFilter filter = new NodeClassFilter (LinkTag.class);
+		LinkStringFilter filter = new LinkStringFilter (prefix)
+		NodeList list = new NodeList();
+		list = parser.extractAllNodesThatMatch (filter);
+		//parser.parse (new HasAttributeFilter ("id"));
+		//parser.parse();
+		//parser.parse(new LinkRegexFilter(prefix+"*",true);
+		/*for (NodeIterator e = parser.elements (); e.hasMoreNodes (); )
+			e.nextNode ().collectInto (list, filter);*/
+		List<String> files = Arrays.asList(list);
+		System.out.println(files);
+		System.out.println("was prefix "+ prefix);
 
-    private int fileHttpCheck(String httpurl, long fileLengthExpected ){
+	    }else{
+	    	File directory= new File (url.substring(0,url.lastIndexOf('/')));
+		FilenameFilter filter = new FilenameFilter() { 
+			public boolean accept(File dir, String name) { 
+				return name.startsWith(prefix); 
+			} 
+		}; 
+		List<String> files = Arrays.asList(directory.listFiles(filter));
+		files.sort();
+		Collections.reverse(files);
+		if (files != null && files.size() > 0) { 
+			//return the last file of the directory
+			return files.get(0);//[files.length-1];
+		}
+		else{
+			return "";
+		}
+	    }
+    }
+
+    public int fileHttpCheck( httpurl, fileLengthExpected ){
 
 	if(fileLengthExpected<1){
 		return FILE_SIZE_EXPECTED_INCORRECT;
@@ -183,13 +179,7 @@ public class Checker{
 
     }
 
-
-    /**
-     * check a file on the filesystem
-     * @param src the path of the file (in java syntax which means which slash like c:/directory/file )
-     * @param fileLenghtExpected a size in number of bytes
-     */
-    private int fileFsCheck(String path, fileLengthExpected ){
+    public int fileFsCheck(String path, fileLengthExpected ){
 
 	if(fileLengthExpected<1){
 		return FILE_SIZE_EXPECTED_INCORRECT 
@@ -199,17 +189,14 @@ public class Checker{
 /*	Date lastModified = new Date(file.lastModified());
  *	System.out.println(lastModified);
  */	
+	
 
 	return fileIntCheck(fileLength, fileLengthExpected);
 
 	}
 
-    /**
-     * check two size in bytes with a errorPercent margin
-     * @param fileLenght a size in number of bytes
-     * @param fileLenghtExpected a size in number of bytes
-     * @return zero if valide, a negative if file is not found, an errorPercent if not valid
-     */
+
+
    private int fileIntCheck(long fileLength, long fileLengthExpected){
 
         if (fileLength <= 0){
@@ -224,5 +211,8 @@ public class Checker{
     	return Math.round(Math.abs(fileLengthExpected/fileLength*100));  
 
     }
+
+
+
 
 }
